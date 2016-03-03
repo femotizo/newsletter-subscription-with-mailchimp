9f0e21e6d1c286eea28e7b589585dd9dbd7ca174
Mario Basic <mario.basic@outlook.com>
2016-03-03 18:19:10 +0100

    The form now works.

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -8,10 +8,46 @@ use App\Http\Requests;
 
 class NewsletterController extends Controller
 {
+    protected $mailchimp;
+
+    public function __construct()
+    {
+        $this->mailchimp = new \Mailchimp(config('services.mailchimp.api_key'));
+    }
+
     public function subscribe(Request $request)
     {
         $this->validate($request, [
             'email' => 'required|email'
         ]);
+
+        $listId = '181710f645'; // Replace this with your own List ID
+
+        try {
+            $this->mailchimp->lists->subscribe($listId, $request->only(['email']));
+        } catch (\Mailchimp_Error $e) {
+            if ($e->getMessage()) {
+                $request->session()->flash('errors',
+                    $this->createViewError('mailchimp_error', $e->getMessage()));
+                return redirect()->back();
+            } else {
+                $request->session()->flash('errors',
+                    $this->createViewError('mailchimp_error', 'An unknown error occurred'));
+                return redirect()->back();
+            }
+        }
+
+    }
+
+    /**
+     * Creates a collection with only one item.
+     *
+     * @param  [type] $key
+     * @param  [type] $value
+     * @return [type]
+     */
+    protected function createViewError($key, $value)
+    {
+        return collect([$key => $value]);
     }
 }
